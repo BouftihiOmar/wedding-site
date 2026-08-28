56a5b7727c994195a92c2d07ee8ea8b130c15da0
BouftihiOmar <65837555+BouftihiOmar@users.noreply.github.com>
2026-08-28 09:01:42 +0100

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://fydfnwcteqmonfsurppw.supabase.co'
+
 const supabaseAnonKey = 'sb_publishable_HVbajdSQvZl46XR_H39n8A_8QfUAv-h'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
